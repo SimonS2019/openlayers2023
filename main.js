52fbaf398cb873268e79edca2d1ed2ee58715162
SimonS2019 <shentiexiong@gmail.com>
2023-10-09 12:28:27 +1300

    6-view

--- main.js
+++ main.js
@@ -2,8 +2,11 @@ window.onload = init;
 function init(){
   const map = new ol.Map({
     view: new ol.View({
-      center: [0, 0],
-      zoom: 2
+      center: [-11957828, 7979541],
+      zoom: 3,
+      maxZoom: 6,
+      minZoom: 2,
+      rotation: 0.5
     }),
     layers: [
       new ol.layer.Tile({
