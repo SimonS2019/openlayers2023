a7a6ea2c1d3752c5816e89a6691b47c0fa7a83d2
SimonS2019 <shentiexiong@gmail.com>
2023-10-10 10:42:16 +1300

    17-Managing Multi-Layers Part 1

--- main.js
+++ main.js
@@ -1,47 +1,47 @@
 window.onload = init;
 
 function init(){
+  // Attribution Control
+  const attributionControl = new ol.control.Attribution({
+    collapsible: true
+  })
+
+  // Map object
   const map = new ol.Map({
     view: new ol.View({
       center: [0, 0],
-      zoom: 3,
-      //extent: [12400753.576694038, -5658730.000549673, 17174426.336716905, -980228.5067132516]
-    }),
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM(),
-        zIndex: 1,
-        visible: false,
-        extent: [12400753.576694038, -5658730.000549673, 17174426.336716905, -980228.5067132516],
-        opacity: 0.1
-      })
-    ],
-    target: 'js-map'
+      zoom: 3,     
+    }),    
+    target: 'js-map',
+    controls: ol.control.defaults({attribution: false}).extend([attributionControl])
   })
 
-  // Layer Group
-  const layerGroup = new ol.layer.Group({
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM({
-          url: 'https://{a-c}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
-        }),
-        zIndex: 0,
-        visible: false,
-        extent: [12400753.576694038, -5658730.000549673, 17174426.336716905, -980228.5067132516],
-        opacity: 0.1
-      }),
-      // Bing Maps Basemap Layer
-      new ol.layer.Tile({
-        source: new ol.source.BingMaps({
-          key: "Your Bingmaps API Key Here",
-          imagerySet: 'CanvasGray'  // Road, CanvasDark, CanvasGray
-        }),
-        visible: false
-      })
-    ]
+  // Base Layers
+  // Openstreet Map Standard
+  const openstreetMapStandard = new ol.layer.Tile({
+    source: new ol.source.OSM(),    
+    visible: true,
+    title: 'OSMStandard'        
   })
-  map.addLayer(layerGroup);
+
+  // Openstreet Map Humanitarian
+  const openstreetMapHumanitarian = new ol.layer.Tile({
+    source: new ol.source.OSM({
+      url: 'https://{a-c}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+    }),
+    visible: false,
+    title: 'OSMHumanitarian'
+  })
+
+  // Bing Maps Basemap Layer
+  const bingMaps = new ol.layer.Tile({
+    source: new ol.source.BingMaps({
+      key: "Your Bingmaps API Key Here",
+      imagerySet: 'CanvasGray'  // Road, CanvasDark, CanvasGray
+    }),
+    visible: false,
+    title: 'BingMaps'
+  })
 
   // CartoDB BaseMap Layer
   const cartoDBBaseLayer = new ol.layer.Tile({
@@ -49,45 +49,66 @@ function init(){
       url: 'http://{1-4}.basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}.png',
       attributions: '© CARTO'
     }),
-    visible: false
+    visible: false,
+    title: 'CartoDarkAll'
   })
-  map.addLayer(cartoDBBaseLayer);
+
+  // Stamen basemap layer
+  const StamenTerrainWithLabels = new ol.layer.Tile({
+    source: new ol.source.Stamen({
+      layer: 'terrain-labels',
+      attributions: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+    }),
+    visible: false,
+    title: 'StamenTerrainWithLabels'
+  })
+  
+  const StamenTerrain = new ol.layer.Tile({
+    source: new ol.source.XYZ({
+      url: 'http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg',
+      attributions: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+    }),
+    visible: false,
+    title: 'StamenTerrain'
+  })
+
+  // Layer Group
+  const baseLayerGroup = new ol.layer.Group({
+    layers: [
+      openstreetMapStandard, openstreetMapHumanitarian, bingMaps, cartoDBBaseLayer,
+      StamenTerrainWithLabels, StamenTerrain
+    ]
+  })
+  map.addLayer(baseLayerGroup);
+
+  // Layer Switcher Logic for BaseLayers
+  const baseLayerElements = document.querySelectorAll('.sidebar > input[type=radio]')
+  for(let baseLayerElement of baseLayerElements){
+    baseLayerElement.addEventListener('change', function(){
+      let baseLayerElementValue = this.value;
+      baseLayerGroup.getLayers().forEach(function(element, index, array){
+        let baseLayerName = element.get('title');
+        element.setVisible(baseLayerName === baseLayerElementValue) 
+      })
+    })
+  }
+  
 
   // TileDebug
   const tileDebugLayer = new ol.layer.Tile({
     source: new ol.source.TileDebug(),
     visible: false
   })
-  map.addLayer(tileDebugLayer);
-
-  // Stamen basemap layer
-  const stamenBaseLayer = new ol.layer.Tile({
-    source: new ol.source.Stamen({
-      layer: 'terrain-labels',
-      attributions: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
-    }),
-    visible: false
-  })
-
-  map.addLayer(stamenBaseLayer);
-
-  const stamenBaseMapLayer = new ol.layer.Tile({
-    source: new ol.source.XYZ({
-      url: 'http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg',
-      attributions: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
-    }),
-    visible: true
-  })
-  map.addLayer(stamenBaseMapLayer);
 
   // tile ArcGIS REST API Layer
   const tileArcGISLayer = new ol.layer.Tile({
     source: new ol.source.TileArcGISRest({
-      url: "http://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Louisville/LOJIC_LandRecords_Louisville/MapServer"
+      url: "http://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Louisville/LOJIC_LandRecords_Louisville/MapServer",
+      attributions: 'Copyright© 2008, MSD, PVA, Louisville Water Company, Louisville Metro Government'
     }),
     visible: false
   })
-  map.addLayer(tileArcGISLayer);
+  
 
   // NOAA WMS Layer
   const NOAAWMSLayer = new ol.layer.Tile({
@@ -99,9 +120,9 @@ function init(){
         TRANSPARENT: true
       },
       attributions: '<a href=https://nowcoast.noaa.gov/>© NOAA<a/>'
-    })
+    }),
+    visible: false
   })
-  map.addLayer(NOAAWMSLayer);
 }
 
 
