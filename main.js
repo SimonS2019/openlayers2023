3b833f93bb784954ce775e90a42c21cefa57ec26
SimonS2019 <shentiexiong@gmail.com>
2023-10-09 13:51:51 +1300

    8- overlays

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ window.onload = init;
 function init(){
   const map = new ol.Map({
     view: new ol.View({
-      center: [-11957828, 7979541],
-      zoom: 3,
+      center: [-12080385, 7567433],
+      zoom: 3, 
       maxZoom: 6,
       minZoom: 2,
-      rotation: 0.5
+      rotation: 0.5 
     }),
     layers: [
       new ol.layer.Tile({
@@ -15,5 +15,21 @@ function init(){
     ],
     target: 'js-map'
   })
+
+  const popupContainerElement = document.getElementById('popup-coordinates');
+  const popup = new ol.Overlay({
+    element: popupContainerElement,
+    positioning: 'top-right'
+  })
+
+  map.addOverlay(popup);
+
+  map.on('click', function(e){
+    const clickedCoordinate = e.coordinate;
+    popup.setPosition(undefined);
+    popup.setPosition(clickedCoordinate);
+    popupContainerElement.innerHTML = clickedCoordinate;
+  })
+
 }
 
